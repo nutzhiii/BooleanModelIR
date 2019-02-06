5f5dff6d423d53558dadefcc0982c4400b324afe
nutzhiii <tanutchapanthum2018@gmail.com>
2019-02-06 10:00:46 +0700

    intial project

---
+++ BooleanModelIR/BooleanModelIR/BooleanModelIR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanModelIR
+{
+    class BooleanModelIR
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello test");
+            Console.ReadKey();
+        }
+    }
+}
